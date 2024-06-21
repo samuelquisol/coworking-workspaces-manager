f43034fc55d7e9e9f5507da3b1486fdc99c2b4f4
Samuel Quintero Solís <samuelquisol@gmail.com>
2024-06-21 07:45:47 -0500

    Workspaces Module Working

--- src/modules/workspaces/workspaces.module.ts
+++ src/modules/workspaces/workspaces.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
+import { Workspaces } from './entities/workspace.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Workspaces])],
   controllers: [WorkspacesController],
   providers: [WorkspacesService],
 })
